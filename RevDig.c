ea6bb6c07d0ca87cd7d1ba788fd5ad28c153912f
adityag-gitam <asatishk@student.gitam.edu>
2024-10-04 05:13:58 +0000

    NEW PROGRAM

---
+++ RevDig.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a=1, b=0, r, z=0;
+    while (a) {
+        printf("enter a number:");
+        scanf("%d",&a);
+        z= z*10 + a;
+    }
+    a=z;
+    while (a>0) {
+        r=a%10;
+        b=b*10 + r;
+        a=a/10;
+    }
+    printf("%d is Yournumber\n",z);
+    printf("%d is REVERSE of %d",b,z);
+    return 0;
+}
+
+/*
+enter a number:1
+enter a number:2
+enter a number:3
+enter a number:4
+enter a number:5
+enter a number:0
+123450 is Yournumber
+54321 is REVERSE of 123450
+*/
